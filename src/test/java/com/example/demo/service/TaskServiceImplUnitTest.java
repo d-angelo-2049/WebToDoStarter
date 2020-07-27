8f4ecfa51b47e4d5eebfe03cdbb1131af6a83120
chandai <63720046+chandai-hardcoregeek@users.noreply.github.com>
2020-07-27 23:49:38 +0900

    add task service (#4)

--- src/test/java/com/example/demo/service/TaskServiceImplUnitTest.java
+++ src/test/java/com/example/demo/service/TaskServiceImplUnitTest.java
@@ -24,102 +24,100 @@ import com.example.demo.repository.TaskDao;
 @ExtendWith(MockitoExtension.class)
 @DisplayName("TaskServiceImplの単体テスト")
 class TaskServiceImplUnitTest {
-	
 
-	
-    @Mock // モック(stub)クラス ダミーオブジェクト
-    private TaskDao dao;
 
-    @InjectMocks // テスト対象クラス　モックを探す newする
-    private TaskServiceImpl taskServiceImpl;
-    
-    @Test // テストケース
-    @DisplayName("テーブルtaskの全件取得で0件の場合のテスト")
-        // テスト名
-    void testFindAllReturnEmptyList() {
-    	
-    	//空のリスト
-    	List<Task> list = new ArrayList<>();
-    	 	
-        // モッククラスのI/Oをセット（findAll()の型と異なる戻り値はNG）
-        when(dao.findAll()).thenReturn(list);
+  @Mock // モック(stub)クラス ダミーオブジェクト
+  private TaskDao dao;
 
-        // サービスを実行
-        List<Task> actualList= taskServiceImpl.findAll();
+  @InjectMocks // テスト対象クラス　モックを探す newする
+  private TaskServiceImpl taskServiceImpl;
 
-        // モックの指定メソッドの実行回数を検査
-        verify(dao, times(1)).findAll();
+  @Test // テストケース
+  @DisplayName("テーブルtaskの全件取得で0件の場合のテスト")
+    // テスト名
+  void testFindAllReturnEmptyList() {
 
-        // 戻り値の検査(expected, actual)
-        assertEquals(0, actualList.size());
-        
-    }
-    
-    @Test // テストケース
-    @DisplayName("テーブルTaskの全件取得で2件の場合のテスト")
-        // テスト名
-    void testFindAllReturnList() {
-    	
-    	//モックから返すListに2つのTaskオブジェクトをセット
-    	List<Task> list = new ArrayList<>();
-    	Task task1 = new Task();
-    	Task task2 = new Task();
-    	list.add(task1);
-    	list.add(task2);
-    	
-    	
-        // モッククラスのI/Oをセット（findAll()の型と異なる戻り値はNG）
-        when(dao.findAll()).thenReturn(list);
+    //空のリスト
+    List<Task> list = new ArrayList<>();
 
-        // サービスを実行
-        List<Task> actualList= taskServiceImpl.findAll();
+    // モッククラスのI/Oをセット（findAll()の型と異なる戻り値はNG）
+    when(dao.findAll()).thenReturn(list);
 
-        // モックの指定メソッドの実行回数を検査
-        verify(dao, times(1)).findAll();
+    // サービスを実行
+    List<Task> actualList = taskServiceImpl.findAll();
 
-        // 戻り値の検査(expected, actual)
-        assertEquals(2, actualList.size());
-        
-    }
+    // モックの指定メソッドの実行回数を検査
+    verify(dao, times(1)).findAll();
 
-    @Test // テストケース
-    @DisplayName("タスクが取得できない場合のテスト")
-        // テスト名
-    void testGetTaskThrowException() {
-    	
-        // モッククラスのI/Oをセット
-        
-        //タスクが取得できないとTaskNotFoundExceptionが発生することを検査
-        
-    }
-    
-    @Test // テストケース
-    @DisplayName("タスクを1件取得した場合のテスト")
-        // テスト名
-    void testGetTaskReturnOne() {
-    	
-    	//Taskをデフォルト値でインスタンス化
-    	
-        // モッククラスのI/Oをセット
+    // 戻り値の検査(expected, actual)
+    assertEquals(0, actualList.size());
 
-        // サービスを実行
+  }
 
-        // モックの指定メソッドの実行回数を検査
+  @Test // テストケース
+  @DisplayName("テーブルTaskの全件取得で2件の場合のテスト")
+    // テスト名
+  void testFindAllReturnList() {
 
-        //Taskが存在していることを確認
-        
-    }
-    
-    @Test // テストケース　ユニットテストではデータベースの例外は考えない
-    @DisplayName("削除対象が存在しない場合、例外が発生することを確認するテスト")
-        // テスト名
-    void throwNotFoundException() {
-    	
-        // モッククラスのI/Oをセット
+    //モックから返すListに2つのTaskオブジェクトをセット
+    List<Task> list = new ArrayList<>();
+    Task task1 = new Task();
+    Task task2 = new Task();
+    list.add(task1);
+    list.add(task2);
 
-    	//削除対象が存在しない場合、例外が発生することを検査
+    // モッククラスのI/Oをセット（findAll()の型と異なる戻り値はNG）
+    when(dao.findAll()).thenReturn(list);
 
-    }
-    
-    
+    // サービスを実行
+    List<Task> actualList = taskServiceImpl.findAll();
+
+    // モックの指定メソッドの実行回数を検査
+    verify(dao, times(1)).findAll();
+
+    // 戻り値の検査(expected, actual)
+    assertEquals(2, actualList.size());
+
+  }
+
+  @Test // テストケース
+  @DisplayName("タスクが取得できない場合のテスト")
+    // テスト名
+  void testGetTaskThrowException() {
+
+    // モッククラスのI/Oをセット
+
+    //タスクが取得できないとTaskNotFoundExceptionが発生することを検査
+
+  }
+
+  @Test // テストケース
+  @DisplayName("タスクを1件取得した場合のテスト")
+    // テスト名
+  void testGetTaskReturnOne() {
+
+    //Taskをデフォルト値でインスタンス化
+
+    // モッククラスのI/Oをセット
+
+    // サービスを実行
+
+    // モックの指定メソッドの実行回数を検査
+
+    //Taskが存在していることを確認
+
+  }
+
+  @Test // テストケース　ユニットテストではデータベースの例外は考えない
+  @DisplayName("削除対象が存在しない場合、例外が発生することを確認するテスト")
+    // テスト名
+  void throwNotFoundException() {
+
+    // モッククラスのI/Oをセット
+
+    //削除対象が存在しない場合、例外が発生することを検査
+
+  }
+
+
 }
